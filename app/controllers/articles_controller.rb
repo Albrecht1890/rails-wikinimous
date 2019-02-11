12b90363a579da63c8f81d2e57ce911d33157bb3
Albrecht Bernstorff <albrecht.bernstorff@web.de>
2019-02-11 18:49:09 +0200

    added bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,47 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    # GET /restaurants/:id
+  end
+
+  def new
+    # GET /restaurants/new
+    @article = Article.new
+  end
+
+  def create
+    # POST /restaurants
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to articles_path
+  end
+
+  def edit
+    # GET /restaurants/:id/edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    # PATCH /restaurants/:id
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    # DELETE /restaurants/:id
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
